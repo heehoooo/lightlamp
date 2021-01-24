96c19d3dfd57611317b8d3652c5d7a0a7238cb94
heehoooo <74821033+heehoooo@users.noreply.github.com>
2021-01-24 21:35:32 +0900

    서랍 여는 기능 구현

---
+++ Assets/Prefabs/closedrawer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class closedrawer : MonoBehaviour
+{
+    public drawer firstdrawer;
+    public GameObject drawer1, drawer2, drawer3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        drawer1 = GameObject.Find("Drawer1");
+        drawer2 = GameObject.Find("Drawer2");
+        drawer3 = GameObject.Find("Drawer3");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(firstdrawer.moveMode == true && firstdrawer.isEnter == true && Input.GetMouseButtonDown(0))
+        {
+            firstdrawer.moveMode = false;
+            drawer1.transform.localPosition = Vector3.MoveTowards(drawer1.transform.localPosition, new Vector3(0.1f, drawer1.transform.localPosition.y, drawer1.transform.localPosition.z), 1f);
+        }
+    }
+}
---
+++ Assets/Prefabs/drawer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drawer : MonoBehaviour
+{
+    public GameObject motherDrawer;
+    public GameObject player;
+    public float speed;
+
+    public float maxPosition;
+
+    public bool isEnter;
+    public bool moveMode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        motherDrawer = GameObject.FindGameObjectWithTag("MotherDrawer");
+        moveMode = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && isEnter == true && moveMode == false)
+        {
+            moveMode = true;
+        }
+        if(moveMode == true && transform.localPosition.x < maxPosition)
+        {
+            transform.Translate(new Vector3(1, 0, 0) * speed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Player")
+        {
+            isEnter = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.collider.tag == "Player")
+        {
+            isEnter = false;
+        }
+    }
+}
